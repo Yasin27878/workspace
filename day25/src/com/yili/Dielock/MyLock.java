5281a38a14df2273f9efbf35925022daa82cfb7f
yili270 <yili270@163.com>
2014-12-25 15:33:50 +0800

    dielock

---
+++ day25/src/com/yili/Dielock/MyLock.java
@@ -0,0 +1,8 @@
+package com.yili.Dielock;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+public class MyLock {
+	public static final Object objA=new Object();
+	public static final Object objB=new Object();
+}
